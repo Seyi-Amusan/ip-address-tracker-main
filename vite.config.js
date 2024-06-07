e387423356cf06b6fcc550f16cbba7ffbac45359
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-06-07 18:45:09 +0100

    Fixing import.meta.env type error on live site

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,42 @@
-// import { defineConfig } from 'vite';
+// // import { defineConfig } from 'vite';
 
-// export default defineConfig({
-//   base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
+// // export default defineConfig({
+// //   base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
+// // });
+
+
+// import { defineConfig, loadEnv } from 'vite';
+
+// export default defineConfig(({ command, mode }) => {
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '');
+  
+//   // Define your Vite configuration
+//   return {
+//     // Base configuration
+//     base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
+
+//     // Other Vite config options
+//     define: {
+//       // Your other define options
+//     },
+//   };
 // });
 
 
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  // Define your Vite configuration
+  const env = loadEnv(mode, process.cwd(), '')
   return {
-    // Base configuration
+    // vite config
     base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
-
-    // Other Vite config options
+    
     define: {
-      // Your other define options
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-  };
-});
+  }
+})
