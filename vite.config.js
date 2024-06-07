ed2ce36249c75ab9bcb45d3acfe20b0f3b4fb2ff
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-06-07 15:59:35 +0100

    Fixing import.meta.env type error on live site

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // Load environment variables from .env files
 export default defineConfig({
-  base: '/your-repo-name/', // Replace with your GitHub repository name
+  base: '/ip-address-tracker-main/', // Replace with your GitHub repository name
   define: {
     // Environment variables will be loaded automatically
   },
