9522af6e06f0ce0bcd32f13951da418205a61edc
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-06-07 15:56:38 +0100

    Fixing import.meta.env type error on live site

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config(); // Load environment variables from .env file in non-production environments
-}
-
+// Load environment variables from .env files
 export default defineConfig({
-  base: '/ip-address-tracker-main/', // Replace with your GitHub repository name
+  base: '/your-repo-name/', // Replace with your GitHub repository name
   define: {
-    'import.meta.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+    // Environment variables will be loaded automatically
   },
 });
