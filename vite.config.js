50b176b04d31c0f2887c06c31b930a368f888e5a
Seyi Amusan <seyijoshua14@gmail.comgit config --global user.email seyijoshua14@gmail.com>
2024-06-08 01:33:31 +0100

    Fixing import.meta.env type error on live site

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,13 @@
-// // import { defineConfig } from 'vite';
-
-// // export default defineConfig({
-// //   base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
-// // });
-
-
-// import { defineConfig, loadEnv } from 'vite';
-
-// export default defineConfig(({ command, mode }) => {
-//   // Load env file based on `mode` in the current working directory.
-//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-//   const env = loadEnv(mode, process.cwd(), '');
-  
-//   // Define your Vite configuration
-//   return {
-//     // Base configuration
-//     base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
-
-//     // Other Vite config options
-//     define: {
-//       // Your other define options
-//     },
-//   };
-// });
-
-
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    // Base configuration
-    base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
+  const env = loadEnv(mode, process.cwd());
 
-    // Other Vite config options
+  return {
+    base: '/ip-address-tracker-main/', // Ensure this matches your GitHub repository name
     define: {
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY)
+      'process.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY),
     },
   };
 });
-
